f58a1ccb18b3c691c1b7ff76b5a12d4039c9e03c
lnangong <lnangong@hawk.iit.edu>
2014-11-22 23:49:04 -0600

    scheduler inprogress...

--- cloudclient/src/cloudclient/Client.java
+++ cloudclient/src/cloudclient/Client.java
@@ -1,93 +1,36 @@
-/**
- * Copyright (c) 2014, Long Nangong. All rights reserved.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in 
- * compliance with the License. You may obtain a copy of the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software distributed under the License is 
- * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
- * express or implied. See the License for the specific language governing permissions and limitations 
- * under the License.
- */
-
 package cloudclient;
 
-import com.amazonaws.AmazonClientException;
-import com.amazonaws.AmazonServiceException;
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.profile.ProfileCredentialsProvider;
-import com.amazonaws.regions.Region;
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.sqs.AmazonSQS;
-import com.amazonaws.services.sqs.AmazonSQSClient;
-import com.amazonaws.services.sqs.model.CreateQueueRequest;
-import com.amazonaws.services.sqs.model.SendMessageRequest;
+import java.net.*;
+import java.io.*;
+
 
 public class Client {
+	
 	public static void main(String[] args) throws Exception {
+		
+		String IP_ADDR = args[0];
+		int PORT = Integer.parseInt(args[1]);
+		
+		try {
+			/* make connection to server socket */
+			Socket socket = new Socket(IP_ADDR, PORT);
+			
+			PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
+			
+			InputStream in = socket.getInputStream();
+			BufferedReader bin = new BufferedReader(new InputStreamReader(in));
+			/* read the date from the socket */
+			String line;
+			while ( (line = bin.readLine()) != null)
+				System.out.println(line);
+			
+			/* close the socket connection*/
+			socket.close();
+		}
 
-        /*
-         * The ProfileCredentialsProvider will return your [default]
-         * credential profile by reading from the credentials file located at
-         * (~/.aws/credentials).
-         */
-        AWSCredentials credentials = null;
-        try {
-            credentials = new ProfileCredentialsProvider().getCredentials();
-        } catch (Exception e) {
-            throw new AmazonClientException(
-                    "Cannot load the credentials from the credential profiles file. " +
-                    "Please make sure that your credentials file is at the correct " +
-                    "location (~/.aws/credentials), and is in valid format.",
-                    e);
-        }
-        
-        AmazonSQS sqs = new AmazonSQSClient(credentials);
-        Region usEast1 = Region.getRegion(Regions.US_EAST_1);
-		sqs.setRegion(usEast1);
-
-        System.out.println("===========================================");
-        System.out.println("Getting Started with Amazon SQS");
-        System.out.println("===========================================\n");
-       
-        String task_1 = "Sleep 10000"; 
-        String task_2 = "Sleep 5000";
-        try {
-            // Create a queue
-            System.out.println("Creating a new SQS queue called JobQueue.\n");
-            CreateQueueRequest createQueueRequest = new CreateQueueRequest("JobQueue");
-            String jobQueueUrl = sqs.createQueue(createQueueRequest).getQueueUrl();
-
-            // List queues
-            System.out.println("Listing all queues in your account.\n");
-            for (String queueUrl : sqs.listQueues().getQueueUrls()) {
-                System.out.println("  QueueUrl: " + queueUrl);
-            }
-            System.out.println();
-            
-        	// Send a message
-            System.out.println("Sending a message to MyQueue.\n");
-            sqs.sendMessage(new SendMessageRequest(jobQueueUrl, task_1));
-            sqs.sendMessage(new SendMessageRequest(jobQueueUrl, task_2));
-            sqs.sendMessage(new SendMessageRequest(jobQueueUrl, task_1));
-            sqs.sendMessage(new SendMessageRequest(jobQueueUrl, task_2));
-            
-        } catch (AmazonServiceException ase) {
-            System.out.println("Caught an AmazonServiceException, which means your request made it " +
-                    "to Amazon SQS, but was rejected with an error response for some reason.");
-            System.out.println("Error Message:    " + ase.getMessage());
-            System.out.println("HTTP Status Code: " + ase.getStatusCode());
-            System.out.println("AWS Error Code:   " + ase.getErrorCode());
-            System.out.println("Error Type:       " + ase.getErrorType());
-            System.out.println("Request ID:       " + ase.getRequestId());
-        } catch (AmazonClientException ace) {
-            System.out.println("Caught an AmazonClientException, which means the client encountered " +
-                    "a serious internal problem while trying to communicate with SQS, such as not " +
-                    "being able to access the network.");
-            System.out.println("Error Message: " + ace.getMessage());
-        }
+		catch (IOException ioe) {
+			System.err.println(ioe);
+		}
             
 	}
 }
