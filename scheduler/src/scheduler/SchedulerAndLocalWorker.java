1156e5deeaba8d7560a9e90e0dbbc071da0c4bf8
lnangong <lnangong@hawk.iit.edu>
2014-11-22 23:44:20 -0600

    scheduler inprogress...

---
+++ scheduler/src/scheduler/SchedulerAndLocalWorker.java
@@ -0,0 +1,108 @@
+package scheduler;
+
+import java.net.*;
+import java.io.*;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClient;
+import com.amazonaws.services.sqs.model.CreateQueueRequest;
+import com.amazonaws.services.sqs.model.SendMessageRequest;
+
+
+public class SchedulerAndLocalWorker {
+	public static void main(String[] args){
+		
+		int PORT = Integer.parseInt(args[0]);
+		
+		try {
+			ServerSocket socket = new ServerSocket(PORT);
+			/* now listen for connections */
+			while (true) {
+				Socket client = socket.accept();
+				PrintWriter pout = new PrintWriter(client.getOutputStream(), true);
+				/* write the Date to the socket */
+				pout.println(new java.util.Date().toString());
+				
+				/* close the socket and resume */
+				/* listening for connections */
+				client.close();
+			}
+		}
+
+		catch (IOException ioe) {
+			System.err.println(ioe);
+		}
+		
+		
+		
+		
+		/*
+         * The ProfileCredentialsProvider will return your [default]
+         * credential profile by reading from the credentials file located at
+         * (~/.aws/credentials).
+         */
+        /*AWSCredentials credentials = null;
+        try {
+            credentials = new ProfileCredentialsProvider().getCredentials();
+        } catch (Exception e) {
+            throw new AmazonClientException(
+                    "Cannot load the credentials from the credential profiles file. " +
+                    "Please make sure that your credentials file is at the correct " +
+                    "location (~/.aws/credentials), and is in valid format.",
+                    e);
+        }
+        
+        AmazonSQS sqs = new AmazonSQSClient(credentials);
+        Region usEast1 = Region.getRegion(Regions.US_EAST_1);
+		sqs.setRegion(usEast1);
+
+        System.out.println("===========================================");
+        System.out.println("Getting Started with Amazon SQS");
+        System.out.println("===========================================\n");
+       
+        String task_1 = "Sleep 10000"; 
+        String task_2 = "Sleep 5000";
+        try {
+            // Create a queue
+            System.out.println("Creating a new SQS queue called JobQueue.\n");
+            CreateQueueRequest createQueueRequest = new CreateQueueRequest("JobQueue");
+            String jobQueueUrl = sqs.createQueue(createQueueRequest).getQueueUrl();
+
+            // List queues
+            System.out.println("Listing all queues in your account.\n");
+            for (String queueUrl : sqs.listQueues().getQueueUrls()) {
+                System.out.println("  QueueUrl: " + queueUrl);
+            }
+            System.out.println();
+            
+        	// Send a message
+            System.out.println("Sending a message to MyQueue.\n");
+            sqs.sendMessage(new SendMessageRequest(jobQueueUrl, task_1));
+            sqs.sendMessage(new SendMessageRequest(jobQueueUrl, task_2));
+            sqs.sendMessage(new SendMessageRequest(jobQueueUrl, task_1));
+            sqs.sendMessage(new SendMessageRequest(jobQueueUrl, task_2));
+            
+        } catch (AmazonServiceException ase) {
+            System.out.println("Caught an AmazonServiceException, which means your request made it " +
+                    "to Amazon SQS, but was rejected with an error response for some reason.");
+            System.out.println("Error Message:    " + ase.getMessage());
+            System.out.println("HTTP Status Code: " + ase.getStatusCode());
+            System.out.println("AWS Error Code:   " + ase.getErrorCode());
+            System.out.println("Error Type:       " + ase.getErrorType());
+            System.out.println("Request ID:       " + ase.getRequestId());
+        } catch (AmazonClientException ace) {
+            System.out.println("Caught an AmazonClientException, which means the client encountered " +
+                    "a serious internal problem while trying to communicate with SQS, such as not " +
+                    "being able to access the network.");
+            System.out.println("Error Message: " + ace.getMessage());
+        }
+            */
+	
+	}
+}
